8b961521c6c9af2ab1dac37c564fca28e39e9acf
farheen <farheenbalouch25>
2025-09-05 14:52:02 +0500

    update my files

---
+++ app.js
@@ -0,0 +1,332 @@
+
+
+// chapter 01 
+
+
+//  1)
+// alert()
+
+//  2)
+// alert("Error! Please enter a valid password")
+
+// 3)
+// alert("Welcome to JS land \n Happy Coding! ")
+
+//04
+// alert("Welcome to JS land")
+
+//  alert("Happy coding!")
+
+//05
+// console.log("Hello...i can run JS through my web browser's console")
+
+//06
+// alert("Hey i am using alert in html and css project 🥳")
+
+//chapter 02
+
+// 1)
+// var username; 
+// console.log(username)
+// 2)
+// var myName = "farheen Balouch";
+// console.log(myName)
+ 
+//03
+// var message = "Hello World"
+// console.log(message)
+
+//04
+// var students_bio= "Jhone Doe"
+// alert(students_bio)
+
+// var students_bio= "15 years old"
+//  alert(students_bio)
+
+// var students_bio= "Certified Mobile Application Development"
+// alert(students_bio)
+
+// 05
+
+// var name="PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(name)
+
+// 06
+// var email= "My email adress is farheenbalouch4@gmail.com"
+// alert(email)
+
+// 07
+
+// var book = "A smarter way to learn JavaScript "
+//  var book2 = "I am trying to learn from the Book "
+// alert(book2+book)
+
+// 08
+// var message = "Yah I can write HTML content through JavaScript"
+// document.write(message)
+
+// 09
+
+// var string ="❝ ■■■■■■■■■■■ஜ۩۞۩ஜ■■■■■■■■■■■ ❞"
+// alert(string)
+
+//end
+
+// chapter 03
+
+//01
+// var age = "I am 20 years old"
+// alert(age)
+
+//02
+
+// var num = "5 times"
+// var num2 ="you have visited this site "
+// alert(num2+num)
+
+//03
+// var birthYear =" My birth year is 2005"
+// var num ="data type of my declared variable is number"
+// document.write(birthYear + "<br>"+ num)
+
+//04
+
+// var visitors_names = "farheen" ,product_title ="perfume", Quantity="5order"
+// document.write(visitors_names+ " "+ product_title + " " +Quantity)
+
+
+// end
+// chapter 4
+
+// 01
+// var num1 ,num2 ,num3;
+
+// 02
+// 5 legal variable
+// var my_age ;
+// var myName;
+// var user_age;
+// var _name;
+// var $age;
+
+// 5 illegal variable
+// var 12name;
+// var @num;
+// var my-age;
+// var !message;
+// var ur color;
+
+
+//   <!-- 3 -->
+// document.write("<h2>Rules for naming JS variables</h2>");
+
+//         document.write("Variable names can only contain letters, numbers, $ and _. <br>");
+//         document.write("For example: <b>$my_1stVariable</b><br><br>");
+
+//         document.write("Variables must begin with a letter, $ or _. <br>");
+//         document.write("For example: <b>$name</b>, <b>_name</b> or <b>name</b><br><br>");
+
+//         document.write("Variable names are case sensitive.<br><br>");
+
+//         document.write("Variable names should not be JS keywords.<br>");
+
+
+// chapter 05
+
+// 01
+// var num1 = 3
+// var num2 = 5
+// document.write( num1+num2)
+
+// 02
+// var num1 = 3
+//  var num2 = 5
+//  document.write( num1*num2)
+// var num1 = 3
+//  var num2 = 5
+//  document.write( num1-num2)
+// var num1 = 3
+//  var num2 = 5
+//  document.write( num1%num2)
+
+// 02
+// a,b
+
+// var num = 4;
+// var dec = "value after variable declaration is :"
+// document.write(dec+num)
+
+
+
+// 02
+// c,d
+// var num1= 8;
+// var text = "Initial value :"
+// document.write(text+num1)
+
+// 02
+// e,f
+// var num = 10;
+// num++;
+// var text ="value after increment is :"
+// document.write(text+num)
+
+// 02
+// g,h
+// var num = 7;
+//  var num2 = 6;
+//  var text ="value after addition is :"
+// document.write(text+(num+num2) )
+
+// i ,j
+// var num = 13;
+// num--;
+// document.write("variable after declaration is :"+num)
+
+// k,l
+// var num = 3;
+// var num2 = 3;
+// document.write(num%num2)
+
+// 06
+// var one_Ticket = 600;
+// var five_Ticket = 5;
+// document.write(one_Ticket*five_Ticket)
+
+// 07
+// var num = 6;
+// document.write("Multiplication Table :");
+
+// for(var i = 1; i <=10; i++) {
+//     document.write(num + "x" +i+ "=" +(num *i) +"<br>" )
+// }
+
+
+// 08
+
+// var F = 70;
+// var C = (F-32)*5/9;
+// document.write("celsius :" +C+ "<br>");
+
+// var c= 25;
+// var f=( c * 9/5)+32;
+// document.write("Fahrenheit: "+f)
+
+
+// 09
+// var  price_item1= 400;
+// var quantity_item1= 3;
+
+// var  price_item2=900;
+// var quantity_item2= 5;
+
+// var Shiping_charges=100;
+
+// var total_item1=price_item1*quantity_item1;
+// var total_item2= price_item2*quantity_item2;
+// var total_cost = total_item1+total_item2+Shiping_charges;
+
+
+// document.write("Total cost of item 1: "+ total_item1 +"<br>")
+// document.write("Total cost of item 2 :" +total_item2  +"<br>")
+// document.write("Shipping charges : " +Shiping_charges   +"<br>")
+// document.write("Final cost : "+total_cost)
+
+
+//10
+
+// var Marks_Obtained = 980;
+// var total_Marks=1100;
+// document.write(Marks_Obtained/total_Marks *100)
+
+
+//11
+// var dollar=10*104.80;
+// var Riyal= 25 * 28;
+// var total_currency=dollar+Riyal;
+// document.write("Total currency in PKR :"+total_currency)
+
+
+// 12
+
+// var num = 5, num1=2,num3=10;
+// document.write("Multiplication: " + (num * num1) + "<br>");
+// document.write("Division: " + (num / num3));
+
+
+//13
+
+// var age = 2025;
+// var birth=  2005;
+// document.write("Your age is : "+age%birth)
+
+//13
+
+// var r= 85;
+// var pi= 3.14;
+
+// var circumfernece = 2 * pi * r;
+// var area= pi* r*r;
+// document.write("The circumference is :" +circumfernece +"<br>")
+// document.write("The area is :"+area)
+
+// 14
+// var snack= "chocolate_chip";
+// var current_age=20;
+// var maximum_age=60;
+// var perday=3;
+// var total_snack=(maximum_age-current_age)*perday*365;
+
+// document.write("you will need "+total_snack+ " " +snack+ " to last until the age of:"+ maximum_age+"  " )
+
+
+// chapter 6-9
+
+// 01
+// var a = 10;
+// ++a,a++ ,--a,a--
+// document.write(a)
+
+// 02
+
+// var  a= 2;
+// var b=1;
+// var result = --a - --b + ++b + b--;
+// document.write(result)
+// --a first decrement 2-1 = 1
+// - --b  first decrement 1-1 =0
+// --a - --b  = 1 - 0= 1
+// +  ++b adition of one in b =1 = b= 2
+//    b-- b= 1 decrement b-- b= 0 
+//    now 2+1 = 3 this output will print first then decrement 
+// a= 1
+// b= 2
+// result = 3
+
+// 03
+
+//  prompt("name")
+   
+// 04
+// var num = prompt("enter a number for multiplication table :" , "5");
+// if (num ===null || num === "" || isNaN (num) ){
+//     num = 5;
+
+// }
+// else{
+//     num = number(num);
+
+// }
+// document.write("multiplication table of "+num )
+// for (var i = 1; i<= 10 ; i++){
+//     document.write(num + " x " + i+ "=" + (num * i)+ "<br>")
+// }
+
+
+
+
+
+
+
+
+
